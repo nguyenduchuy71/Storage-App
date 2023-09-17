4d6351b0d870674269b2ae41e523a6bea48f92fb
nguyenduchuy71 <wtfomg3650@gmail.com>
2023-09-17 09:28:32 +0700

    update code

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import styled from 'styled-components'
-import { enterRoomAsync, enterProjectAsync } from '../features/appSlice'
-import { useDispatch } from 'react-redux'
-import { useHistory } from 'react-router-dom'
+import React from "react";
+import styled from "styled-components";
+import { enterRoomAsync, enterProjectAsync } from "../features/appSlice";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
 import {
   Button,
   CardActionArea,
@@ -13,148 +13,154 @@ import {
   Box,
   Typography,
   Modal,
-  TextField
-} from '@mui/material'
-import { auth } from '../config/firebaseConfig.js'
-import { useAuthState } from 'react-firebase-hooks/auth'
-import { deleteProjectAsync } from '../features/projectSlice'
-import DeleteForeverIcon from '@mui/icons-material/DeleteForever'
-import InputIcon from '@mui/icons-material/Input';
+  TextField,
+} from "@mui/material";
+import { auth } from "../config/firebaseConfig.js";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { deleteProjectAsync } from "../features/projectSlice";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import InputIcon from "@mui/icons-material/Input";
 
 const style = {
-  position: 'absolute',
-  borderRadius: '5px',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
+  position: "absolute",
+  borderRadius: "5px",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
   width: 400,
-  bgcolor: '#ffffff',
-  border: '3px solid #3399ff',
+  bgcolor: "#ffffff",
+  border: "3px solid #3399ff",
   boxShadow: 24,
   p: 4,
-  display: 'flex',
-  flexDirection: 'column'
-}
+  display: "flex",
+  flexDirection: "column",
+};
 
-function ProjectCard (props) {
-  const [user] = useAuthState(auth)
-  const project = props.doc.data()
-  const dispatch = useDispatch()
-  const history = useHistory()
-  const [projectPassword, setProjectPassword] = React.useState('')
-  const [open, setOpen] = React.useState(false)
-  const handleOpen = () => setOpen(true)
-  const handleClose = () => setOpen(false)
+function ProjectCard(props) {
+  const [user] = useAuthState(auth);
+  const project = props.doc.data();
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const [projectPassword, setProjectPassword] = React.useState("");
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   const handleJoinProject = async () => {
-    const project_id = project.project_id
+    const project_id = project.project_id;
     if (project.project_pass === projectPassword) {
       await dispatch(
         await enterRoomAsync({
           roomId: project_id,
-          user: user
+          user: user,
         })
-      )
+      );
       await dispatch(
         await enterProjectAsync({
-          user: user
+          user: user,
         })
-      )
-      setOpen(false)
-      history.push(`/project/${project_id}`)
+      );
+      setOpen(false);
+      history.push(`/project/${project_id}`);
     }
-  }
+  };
 
   const handleDeleteProject = async () => {
     await dispatch(
       deleteProjectAsync({
-        project_id: project.project_id
+        project_id: project.project_id,
       })
-    )
-  }
+    );
+  };
 
   return (
     <MainContainer>
       <Card sx={{ fontSize: 12 }}>
         <CardMedia
-          component='img'
-          height='160'
-          image='https://cdn01.alison-static.net/courses/1658/alison_courseware_intro_1658.jpg'
-          alt='green iguana'
+          component="img"
+          height="160"
+          image="https://cdn01.alison-static.net/courses/1658/alison_courseware_intro_1658.jpg"
+          alt="green iguana"
         />
         <CardActionArea>
           <CardContent>
             <Typography
-              sx={{ textAlign: 'center', cursor: 'default' }}
+              sx={{ textAlign: "center", cursor: "default" }}
               gutterBottom
-              variant='h4'
-              component='div'
+              variant="h4"
+              component="div"
             >
               {project.project_name}
             </Typography>
           </CardContent>
         </CardActionArea>
         <CardActions
-            sx={{
-              display: 'flex',
-              justifyContent: 'space-between',
-              alignItems: 'center'
-            }}>
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
           <Button
-            sx={{ display: 'flex', 
-              justifyContent: 'center', 
-              alignItems: 'center' }}
-            variant='contained'
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+            variant="contained"
             onClick={handleOpen}
           >
             <InputIcon sx={{ fontSize: 24 }} />
           </Button>
           <Button
-              sx={{ display: 'flex', 
-                justifyContent: 'center',
-                alignItems: 'center' }}
-              variant='contained'
-              onClick={handleDeleteProject}
-              color="error"
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+            variant="contained"
+            onClick={handleDeleteProject}
+            color="error"
           >
             <DeleteForeverIcon sx={{ fontSize: 24 }} />
           </Button>
         </CardActions>
       </Card>
-        <Modal
-          open={open}
-          onClose={handleClose}
-          aria-labelledby='modal-modal-title'
-          aria-describedby='modal-modal-description'
-        >
-          <Box sx={style}>
-            <Typography sx={{ fontSize: 16 }} id='modal-modal-title'>
-              Join project now
-            </Typography>
-            <TextField
-              required
-              sx={{ mt: 2, mb: 2 }}
-              id='project_password'
-              fullWidth
-              label='Password'
-              variant='outlined'
-              name='project_password'
-              onChange={e => setProjectPassword(e.target.value)}
-            />
-            <Button
-              sx={{ fontSize: 12 }}
-              onClick={handleJoinProject}
-              variant='contained'
-            >
-              Go
-            </Button>
-          </Box>
-        </Modal>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography sx={{ fontSize: 16 }} id="modal-modal-title">
+            Join project now
+          </Typography>
+          <TextField
+            required
+            sx={{ mt: 2, mb: 2 }}
+            id="project_password"
+            fullWidth
+            label="Password"
+            type="password"
+            variant="outlined"
+            name="project_password"
+            onChange={(e) => setProjectPassword(e.target.value)}
+          />
+          <Button
+            sx={{ fontSize: 12 }}
+            onClick={handleJoinProject}
+            variant="contained"
+          >
+            Go
+          </Button>
+        </Box>
+      </Modal>
     </MainContainer>
-  )
+  );
 }
 
-export default React.memo(ProjectCard)
+export default React.memo(ProjectCard);
 const MainContainer = styled.div`
   padding-bottom: 20px;
-`
+`;
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,227 +1,223 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
-function HomeScreen () {
+function HomeScreen() {
   return (
     <HomeScreenContainer>
-      <section id='hero' className='d-flex align-items-center'>
-        <div className='container'>
-          <div className='row'>
-            <div className='col-lg-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center'>
-              <h1 data-aos='fade-up'>Grow your team</h1>
-              <h2 data-aos='fade-up' data-aos-delay='400'>
+      <section id="hero" className="d-flex align-items-center">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center">
+              <h1 data-aos="fade-up">Grow your team</h1>
+              <h2 data-aos="fade-up" data-aos-delay="400">
                 This website supports your research and data storage using many
                 popular hosting services like Amazon, Firebase, ...
               </h2>
-              <div data-aos='fade-up' data-aos-delay='800'>
-                <a href='/projects' className='btn-get-started scrollto'>
-                  Get Started
-                </a>
+              <div data-aos="fade-up" data-aos-delay="800">
+                <Link to="/projects">
+                  <div className="btn-get-started scrollto">Get Started</div>
+                </Link>
               </div>
             </div>
             <div
-              className='col-lg-6 order-1 order-lg-2 hero-img'
-              data-aos='fade-left'
-              data-aos-delay='200'
+              className="col-lg-6 order-1 order-lg-2 hero-img"
+              data-aos="fade-left"
+              data-aos-delay="200"
             >
               <img
-                src='assets/img/hero-img.png'
-                className='img-fluid animated'
-                alt=''
+                src="assets/img/hero-img.png"
+                className="img-fluid animated"
+                alt=""
               />
             </div>
           </div>
         </div>
       </section>
-      <section id='clients' class='clients clients'>
-        <div class='container'>
-          <div class='row'>
-            <div class='col-lg-2 col-md-4 col-6'>
+      <section id="clients" class="clients clients">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-2 col-md-4 col-6">
               <img
-                src='assets/img/clients/client-1.png'
-                class='img-fluid'
-                alt=''
-                data-aos='zoom-in'
+                src="assets/img/clients/client-1.png"
+                class="img-fluid"
+                alt=""
+                data-aos="zoom-in"
               />
             </div>
 
-            <div class='col-lg-2 col-md-4 col-6'>
+            <div class="col-lg-2 col-md-4 col-6">
               <img
-                src='assets/img/clients/client-2.png'
-                class='img-fluid'
-                alt=''
-                data-aos='zoom-in'
-                data-aos-delay='100'
+                src="assets/img/clients/client-2.png"
+                class="img-fluid"
+                alt=""
+                data-aos="zoom-in"
+                data-aos-delay="100"
               />
             </div>
 
-            <div class='col-lg-2 col-md-4 col-6'>
+            <div class="col-lg-2 col-md-4 col-6">
               <img
-                src='assets/img/clients/client-3.png'
-                class='img-fluid'
-                alt=''
-                data-aos='zoom-in'
-                data-aos-delay='200'
+                src="assets/img/clients/client-3.png"
+                class="img-fluid"
+                alt=""
+                data-aos="zoom-in"
+                data-aos-delay="200"
               />
             </div>
 
-            <div class='col-lg-2 col-md-4 col-6'>
+            <div class="col-lg-2 col-md-4 col-6">
               <img
-                src='assets/img/clients/client-4.png'
-                class='img-fluid'
-                alt=''
-                data-aos='zoom-in'
-                data-aos-delay='300'
+                src="assets/img/clients/client-4.png"
+                class="img-fluid"
+                alt=""
+                data-aos="zoom-in"
+                data-aos-delay="300"
               />
             </div>
 
-            <div class='col-lg-2 col-md-4 col-6'>
+            <div class="col-lg-2 col-md-4 col-6">
               <img
-                src='assets/img/clients/client-5.png'
-                class='img-fluid'
-                alt=''
-                data-aos='zoom-in'
-                data-aos-delay='400'
+                src="assets/img/clients/client-5.png"
+                class="img-fluid"
+                alt=""
+                data-aos="zoom-in"
+                data-aos-delay="400"
               />
             </div>
 
-            <div class='col-lg-2 col-md-4 col-6'>
+            <div class="col-lg-2 col-md-4 col-6">
               <img
-                src='assets/img/clients/client-6.png'
-                class='img-fluid'
-                alt=''
-                data-aos='zoom-in'
-                data-aos-delay='500'
+                src="assets/img/clients/client-6.png"
+                class="img-fluid"
+                alt=""
+                data-aos="zoom-in"
+                data-aos-delay="500"
               />
             </div>
           </div>
         </div>
       </section>
-      <section id='contact' class='contact'>
-        <div class='container'>
-          <div class='section-title' data-aos='fade-up'>
+      <section id="contact" class="contact">
+        <div class="container">
+          <div class="section-title" data-aos="fade-up">
             <h2>Contact Us</h2>
           </div>
 
-          <div class='row'>
+          <div class="row">
             <div
-              class='col-lg-4 col-md-6'
-              data-aos='fade-up'
-              data-aos-delay='100'
+              class="col-lg-4 col-md-6"
+              data-aos="fade-up"
+              data-aos-delay="100"
             >
-              <div class='contact-about'>
+              <div class="contact-about">
                 <h3>Huy Nguyen</h3>
                 <p>Python Developer</p>
-                <div class='social-links'>
+                <div class="social-links">
                   <a
-                    href='https://www.facebook.com/profile.php?id=100034499171734'
-                    target='blank'
-                    class='facebook'
+                    href="https://www.facebook.com/profile.php?id=100034499171734"
+                    target="blank"
+                    class="facebook"
                   >
-                    <i class='bi bi-facebook'></i>
+                    <i class="bi bi-facebook"></i>
                   </a>
                   <a
-                    href='https://github.com/nguyenduchuy71?tab=repositories'
-                    target='blank'
-                    class='github'
+                    href="https://github.com/nguyenduchuy71?tab=repositories"
+                    target="blank"
+                    class="github"
                   >
-                    <i class='bi bi-github'></i>
+                    <i class="bi bi-github"></i>
                   </a>
                   <a
-                    href='https://www.linkedin.com/in/nguyen-huy-26146a14a/'
-                    target='blank'
-                    class='linkedin'
+                    href="https://www.linkedin.com/in/nguyen-huy-26146a14a/"
+                    target="blank"
+                    class="linkedin"
                   >
-                    <i class='bi bi-linkedin'></i>
+                    <i class="bi bi-linkedin"></i>
                   </a>
                 </div>
               </div>
             </div>
 
             <div
-              class='col-lg-3 col-md-6 mt-4 mt-md-0'
-              data-aos='fade-up'
-              data-aos-delay='200'
+              class="col-lg-3 col-md-6 mt-4 mt-md-0"
+              data-aos="fade-up"
+              data-aos-delay="200"
             >
-              <div class='info'>
+              <div class="info">
                 <div>
-                  <i class='ri-map-pin-line'></i>
+                  <i class="ri-map-pin-line"></i>
                   <p>
                     Hiep Binh Chanh, Thu Duc<br></br>TP.Ho Chi Minh
                   </p>
                 </div>
 
                 <div>
-                  <i class='ri-mail-send-line'></i>
+                  <i class="ri-mail-send-line"></i>
                   <p>wtfomg3650@gmail.com</p>
                 </div>
 
                 <div>
-                  <i class='ri-phone-line'></i>
+                  <i class="ri-phone-line"></i>
                   <p>+84 349360265</p>
                 </div>
               </div>
             </div>
 
             <div
-              class='col-lg-5 col-md-12'
-              data-aos='fade-up'
-              data-aos-delay='300'
+              class="col-lg-5 col-md-12"
+              data-aos="fade-up"
+              data-aos-delay="300"
             >
-              <form
-                action='forms/contact.php'
-                method='post'
-                role='form'
-                class='php-email-form'
-              >
-                <div class='form-group'>
+              <form role="form" class="php-email-form">
+                <div class="form-group">
                   <input
-                    type='text'
-                    name='name'
-                    class='form-control'
-                    id='name'
-                    placeholder='Your Name'
+                    type="text"
+                    name="name"
+                    class="form-control"
+                    id="name"
+                    placeholder="Your Name"
                     required
                   />
                 </div>
-                <div class='form-group'>
+                <div class="form-group">
                   <input
-                    type='email'
-                    class='form-control'
-                    name='email'
-                    id='email'
-                    placeholder='Your Email'
+                    type="email"
+                    class="form-control"
+                    name="email"
+                    id="email"
+                    placeholder="Your Email"
                     required
                   />
                 </div>
-                <div class='form-group'>
+                <div class="form-group">
                   <input
-                    type='text'
-                    class='form-control'
-                    name='subject'
-                    id='subject'
-                    placeholder='Subject'
+                    type="text"
+                    class="form-control"
+                    name="subject"
+                    id="subject"
+                    placeholder="Subject"
                     required
                   />
                 </div>
-                <div class='form-group'>
+                <div class="form-group">
                   <textarea
-                    class='form-control'
-                    name='message'
-                    rows='5'
-                    placeholder='Message'
+                    class="form-control"
+                    name="message"
+                    rows="5"
+                    placeholder="Message"
                     required
                   ></textarea>
                 </div>
-                <div class='my-3'>
-                  <div class='loading'>Loading</div>
-                  <div class='error-message'></div>
-                  <div class='sent-message'>
+                <div class="my-3">
+                  <div class="loading">Loading</div>
+                  <div class="error-message"></div>
+                  <div class="sent-message">
                     Your message has been sent. Thank you!
                   </div>
                 </div>
-                <div class='text-center'>
-                  <button type='submit'>Send Message</button>
+                <div class="text-center">
+                  <button type="submit">Send Message</button>
                 </div>
               </form>
             </div>
@@ -229,10 +225,10 @@ function HomeScreen () {
         </div>
       </section>
     </HomeScreenContainer>
-  )
+  );
 }
 
-export default HomeScreen
+export default HomeScreen;
 const HomeScreenContainer = styled.div`
   width: 100%;
-`
+`;
